0025b3b08a5f4f55bdb8414716f6fb54b12b0592
Szwedziooo <jacekszwed321@gmail.com>
2024-11-26 15:37:36 +0100

    Dodanie możliwości zapisu i odczytu konfiguracji oraz punktów ROI z pliku [FIX 3 - zmiana na używanie dynamicznej ścieżki]

--- praca_inzynierska/read_conifg.py
+++ praca_inzynierska/read_conifg.py
@@ -1,7 +1,9 @@
 import json
+import os
 
 def read_config(path):
     print("odczytano")
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path,'r') as file:
         tmp = json.load(file)
         file.close()
--- praca_inzynierska/write_config.py
+++ praca_inzynierska/write_config.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 def write_config(path, variable):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
     with open(path,'w') as file:
         json.dump(variable, file)
         file.close()
