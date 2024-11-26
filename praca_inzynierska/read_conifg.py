f3c4289e3491d1092edc134b5d2c69069183b8d0
Szwedziooo <jacekszwed321@gmail.com>
2024-11-26 15:44:38 +0100

    Dodanie możliwości zapisu i odczytu konfiguracji oraz punktów ROI z pliku [FIX 4 - debugowanie]

--- praca_inzynierska/read_conifg.py
+++ praca_inzynierska/read_conifg.py
@@ -2,9 +2,9 @@ import json
 import os
 
 def read_config(path):
-    print("odczytano")
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path,'r') as file:
         tmp = json.load(file)
         file.close()
+        print(path, " - ", tmp)
         return tmp
