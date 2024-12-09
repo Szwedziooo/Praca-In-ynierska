66e6ad09c14d9401a3063bbb38b7769e82249be0
Szwedziooo <jacekszwed321@gmail.com>
2024-12-09 13:24:14 +0100

    Zmiana kolejności sortowania

--- praca_inzynierska/detect_rq.py
+++ praca_inzynierska/detect_rq.py
@@ -26,6 +26,6 @@ def detect_qr(img, model, margin=10):
                 rois.append((x1, y1, x2 - x1, y2 - y1))
 
     # Sortowanie wyników (opcjonalnie)
-    rois.sort(key=lambda x: (-x[1], x[0]))
+    rois.sort(key=lambda x: (-x[1], -x[0]))
 
     return rois
