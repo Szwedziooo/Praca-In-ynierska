a95ab04aa6bc0f73ba1d8a0d46839b01cb9aa6c9
Szwedziooo <jacekszwed321@gmail.com>
2024-12-17 18:17:18 +0100

    wysyłanie numerów boxów poprzez modbusa [fix6]

--- praca_inzynierska/communication.py
+++ praca_inzynierska/communication.py
@@ -127,7 +127,8 @@ def modbus_TCP_send_holding_registers(plc_ip, default_port, HR_start_idx, values
     try:
         if client.connect():
             print(f"PLC connected via MODBUS")
-            send_result = client.write_registers(HR_start_idx, values)
+            for a,b in HR_start_idx, values:
+                send_result = client.write_registers(a, b)
             if send_result.isError():
                 print("ERROR sending DATA")
                 data_sent = False
